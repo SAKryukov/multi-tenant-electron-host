a95b8a6a95f425545ce971c471d59897878eff93
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2025-09-14 18:14:26 -0400

    Fixed plugin Split

--- Editor/plugins/case5.split.js
+++ Editor/plugins/case5.split.js
@@ -4,15 +4,17 @@ pluginProcessor.registerPlugin({
     name: "Split",
     description: "Split selection into words where the case is changed",
     handler: api => {
+        const isLetter = sample => sample.toLowerCase() != sample.toUpperCase;
         const slice = api.editor.value.slice(api.editor.selectionStart, api.editor.selectionEnd);
         let newSlice = [];
-        let previousLow = null;
+        let previousLow = false;
+        let isPreviousLetter = false;
         for (const letter of slice) {
             const isLow = letter.toLowerCase() == letter;
-            if (previousLow == null)
-                previousLow = isLow;
-            if (previousLow != isLow)
+            if (isPreviousLetter && previousLow && !isLow)
                 newSlice.push(api.blankSpace);
+            previousLow = isLow;
+            isPreviousLetter = isLetter(letter);
             newSlice.push(letter);
         } //loop
         api.editor.setRangeText(newSlice.join(api.empty));
