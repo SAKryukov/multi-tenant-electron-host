f3ce628dce8c9a8a1c2739172828fd4eb38a264f
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2025-09-16 12:22:52 -0400

    Minor fixes improvements: * HTML: shortcut * Plugins: renamed exception in handler demo

--- Editor/plugins/3.demo/6.current-line.exception.js
+++ Editor/plugins/3.demo/6.current-line.exception.js
@@ -1,8 +1,8 @@
 "use strict";
 
 pluginProcessor.registerPlugin({
-    name: "Current Line Under Development",
-    description: "Current line under development demonstrates exception handling by the editor API",
+    name: `Current Line ${String.fromCharCode(0x2014)} Exception Demo`,
+    description: "Current line demonstrates exception handling by the editor API",
     handler: api => {
         point = api.currentLines;
         api.scrollTo(point[0], point[1], true);
