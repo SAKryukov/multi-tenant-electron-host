0d830938b47177e1e75870c888011a9f97957b99
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2025-09-16 10:14:12 -0400

    Plugin: added one more sample plugin

---
+++ Editor/plugins/3.demo/E.list-to-HTML.js
@@ -0,0 +1,29 @@
+"use strict";
+
+pluginProcessor.registerPlugin({
+    name: "List to HTML",
+    description: "Convert selected lines to HTML list",
+    handler: api => {
+        api.pushSelection();
+        const domain = api.currentLines;
+        const lines = api.editor.value.slice(domain[0], domain[1]).split(api.newLine);
+        const starts = [];
+        let position = domain[0];
+        for (const line of lines) {
+            starts.push(position);
+            position += line.length + 1;
+        } //loop
+        for (let index = starts.length - 1; index >= 0; --index) {
+            const start = starts[index];
+            const left = "<li>"; const right = "</li>";
+            const rightPosition = start + lines[index].length;
+            api.editor.setSelectionRange(rightPosition, rightPosition);
+            api.editor.setRangeText(right);
+            api.editor.setSelectionRange(start, start);
+            api.editor.setRangeText(left);
+        } //loop
+        api.popSelection();
+    },
+    isEnabled: api => api.selectionLength > 0,
+    menuItemIndent: demoGroupIndent,
+});
