90bfc1f560a4190888134e840ca8c0de886fa6c0
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2025-09-16 11:29:40 -0400

    Editor API, plugins: added a new plugin to document editor API

---
+++ Editor/plugins/3.demo/show-editor-API.js
@@ -0,0 +1,41 @@
+"use strict";
+
+pluginProcessor.registerPlugin({
+    name: `Show Editor API`,
+    description: "Show editor API as editor text",
+    handler: api => {
+        const keys = {
+            arrow: "=>",
+            brackets: ["(", ")"],
+            function: "function",
+            inBrackets: content => `(${content})`,
+        };
+        const apiItems = [];
+        for (let index in api)  {
+            let name = index;
+            if (api[index] instanceof Function) {
+                const code = api[index].toString();
+                const arrow = code.indexOf(keys.arrow);
+                if (arrow >=0) {
+                    const argumentPart = code.substring(0, arrow).trim();
+                    name += argumentPart.indexOf(keys.brackets[0]) >= 0
+                        ? argumentPart
+                        : keys.inBrackets(argumentPart);
+                } else { // function
+                    const functionWord = code.indexOf(keys.function);
+                    if (functionWord >= 0) {
+                        const functionContent = code.slice(functionWord);
+                        const argumentPart = functionContent.slice(0, functionContent.indexOf(keys.brackets[1]) + 1);
+                        name += argumentPart;
+                    } //if
+                } //if
+            } //loop
+            apiItems.push(name);
+        } //loop
+        apiItems.push(api.newLine);
+        api.editor.value = apiItems.join(api.newLine);
+        api.isModified = true;
+    }, //handler
+    isEnabled: true,
+    menuItemIndent: demoGroupIndent,
+});
