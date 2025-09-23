ff481a23cd58385f046f562ad7d46568a95cc306
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2025-09-23 13:17:14 -0400

    Created new plugin for testing purposes

---
+++ Editor/plugins/3.demo/F.generate-numbered-lines.js
@@ -0,0 +1,17 @@
+"use strict";
+
+pluginProcessor.registerPlugin({
+    name: "Generate Numbered Lines",
+    description: "Generate numbered lines for testing",
+    handler: api => {
+        const content = [];
+        const max = 2 << 14;
+        const pad = max.toString().length;        
+        for (let index = 1; index <= max; ++index)
+            content.push(index.toString().padStart(pad, "0"));
+        api.editor.value = content.join(api.newLine);
+        api.isModified = true;
+    },
+    isEnabled: api => true,
+    menuItemIndent: demoGroupIndent,
+});
