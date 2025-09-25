ca7adbe863d15e2119f237e2dff1b82d5d0e74af
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2025-09-24 20:49:29 -0400

    Status bar: reduced flicker

--- Editor/renderer/renderer.js
+++ Editor/renderer/renderer.js
@@ -5,9 +5,17 @@ const definitionSet = getDefinitionSet();
 window.addEventListener(definitionSet.events.DOMContentLoaded, async () => {
 
     elementSet = getElementSet(document);
+    elementSet.statusBar.cursorPositionIndicator.innerHTML =
+        definitionSet.status.cursorPosition(); // initial default, to reduce flicker
 
     if (window.bridgePlugin)
         window.bridgePlugin.subscribeToPlugin(async (plugins, pluginsKeyword) => {
+            elementSet.editor.addEventListener(definitionSet.events.selectionchange, event => {
+                const start = elementSet.editorAPI.positionToCursor(event.target.selectionStart);
+                const end = elementSet.editorAPI.positionToCursor(event.target.selectionEnd);
+                elementSet.statusBar.cursorPositionIndicator.innerHTML =
+                    definitionSet.status.cursorPosition(start, end);
+            }); //editor.onselectionchange
             const metadata = window.bridgeMetadata.pushedMetadata();
             for (let index = 0; index < plugins.length; ++index) {
                 const option = document.createElement(definitionSet.elements.option);
@@ -22,12 +30,6 @@ window.addEventListener(definitionSet.events.DOMContentLoaded, async () => {
             createEditorAPI(elementSet, searchDialogObject.api);
             subscribe(elementSet, menu, searchDialogObject.searchDialog, metadata);
             pluginProcessor.processPlugins(definitionSet, elementSet, menu, plugins, pluginsKeyword);
-            elementSet.editor.addEventListener(definitionSet.events.selectionchange, event => {
-                const start = elementSet.editorAPI.positionToCursor(event.target.selectionStart);
-                const end = elementSet.editorAPI.positionToCursor(event.target.selectionEnd);
-                elementSet.statusBar.cursorPositionIndicator.innerHTML =
-                    definitionSet.status.cursorPosition(start, end);
-            }); //editor.onselectionchange
         });
 
     if (!window.bridgePlugin)
