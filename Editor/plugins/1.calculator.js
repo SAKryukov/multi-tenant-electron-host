5b5688756812439ed37101bb509e02f1a0fef86b
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2025-10-22 21:59:09 -0400

    Editor plugins: Refined Calculator

--- Editor/plugins/1.calculator.js
+++ Editor/plugins/1.calculator.js
@@ -4,25 +4,60 @@
     name: `Calculator (Ctrl+F5)`,
 
     description: "Calculate expression or function body in the selection.<br/>"
-        + "For output, use console.clear(), console.log() and/or return statement.<br/>"
-        + "If console was not used and return is undefined<br/>"
-        + "&emsp;&emsp;the code will be interpreted as an expression.",
+        + "For output, use console.clear(), console.log() and/or return statement",
 
     handler: api => {
-        const codeReturn = "return";
+        const codeReturn = "return ";
         const codeArgument = "console";
+        const jsonSpace = api.blankSpace.repeat(4);
         const startPoint = api.editor.selectionStart;
         const insertPoint = api.editor.selectionEnd;
         let firstLine = true;
-        let consoleUsed = false;
 
-        //throw new Error(`<p>Object <code style="color: green">window</code> cannot be used in the Calculator code</p>`);
+        const replacementObject = object => { // precondition is: object && object instanceof Object
+            if (object instanceof Array) {
+                const length = object.length;
+                if (length < 1)
+                    return `${object.constructor.name}[]`;
+                return `${object.constructor.name}[${length}]`;
+            } else { //Object
+                const length = Object.keys(object).length;
+                return length > 0
+                    ? `${object.constructor.name} {${String.fromCharCode(0x2026)}}`
+                    : `${object.constructor.name} {}`;
+            } //if
+        }; //replacementObject
+
+        const objectSet = new Set();
+        const decircle = object => { // precondition is: object && object instanceof Object
+            debugger;
+            objectSet.add(object);
+            for (const index in object) {
+                const child = object[index];
+                if (child instanceof Function)
+                    object[index] = child.toString().replaceAll(api.newLine, api.empty);
+                else if (child != null && child instanceof Object) {
+                    if (objectSet.has(child))
+                        object[index] = replacementObject(object[index]);
+                    else
+                        decircle(child);
+                } //if
+            } //loop
+            return object;
+        }; //decircle
+
+        const stringify = object => {
+            objectSet.clear();
+            if (object instanceof Object)
+                return JSON.stringify(decircle(object), null, jsonSpace);
+            return object.toString();
+        }; //stringify
 
         const output = object => {
-            if (!object) return;
+            if (!object) object = api.empty + object;
             const text =
                 (firstLine ? api.newLine : api.empty)
-                + object.toString()
+                + stringify(object)
                 + api.newLine;
             firstLine = false;
             const point = api.editor.selectionEnd; 
@@ -30,13 +65,11 @@
             api.editor.setRangeText(text);
             const newPoint = point + text.length;
             api.scrollTo(newPoint, newPoint);
-            consoleUsed = true;
         }; //output
 
         const clear = () => {
             api.editor.setSelectionRange(insertPoint, api.editor.textLength);
             api.editor.setRangeText(api.empty);
-            consoleUsed = true;
         }; //clear
 
         const consoleApi = {
@@ -51,23 +84,36 @@
         const globalObjects = Object.keys(window);
         const replacedGlobalObjects = Array(globalObjects.length).fill(undefined);
         const runScript = code => {
-            const script = new Function(
-                ...globalObjects,
-                codeArgument,
-                `"use strict";` + api.newLine + code);
-            return script(...replacedGlobalObjects, consoleApi);
+            try {
+                const script = new Function(
+                    ...globalObjects,
+                    codeArgument,
+                    `"use strict";` + api.newLine + code);
+                return [script(...replacedGlobalObjects, consoleApi), null];
+            } catch (error) {
+                return [null, error];
+            } //exception
         }; //runScript
 
         let code = api.selectedText;
-        let result = runScript(code);
-        if ((!consoleUsed) && result === undefined) { //expression
-            code = `${codeReturn} (${code});`;
-            result = runScript(code);
-        } //if expression
-        if (result === undefined)
-            result = api.empty;
+        let functionError = undefined;
+        let expressionError = undefined;
+        let result = undefined;
+        [result, expressionError] =
+            runScript(codeReturn + `(${code.replace(/[;\s]+$/, api.empty)})`); // expression
+        if (expressionError) //function
+            [result, functionError] = runScript(code);
+        if (expressionError && functionError) {
+            if (functionError.message == expressionError.message)
+                throw functionError;
+            else
+                throw new Error("<br/>Both function and expression error:" +
+                    `<br/><br/>If code is interpreted as function body:<br/>${functionError.message}` +
+                    `<br/><br/>If code is interpreted as expression:<br/>${expressionError.message}`);
+        } else if (functionError) //SA???
+            throw new Error(`<br/>Function error:<br/><br/>${functionError.message}`);
         api.scrollTo(api.editor.selectionEnd, api.editor.selectionEnd);
-        api.editor.setRangeText(api.newLine + result + api.newLine);
+        output(result);
         api.scrollTo(startPoint, insertPoint);
     }, //handler
 
